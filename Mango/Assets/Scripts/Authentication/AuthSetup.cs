b28fdcc18aaa6bc1b8e3810eac3dca057baa2ff8
Alberto Castañeda <albcastar@gmail.com>
2020-10-06 18:38:17 -0500

    Primera interfaz de registrar usuario

---
+++ Mango/Assets/Scripts/Authentication/AuthSetup.cs
@@ -0,0 +1,31 @@
+﻿using Firebase;
+using Firebase.Extensions;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AuthSetup : MonoBehaviour
+{
+
+    public AuthManager authManager;
+
+    private DependencyStatus dependencyStatus;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task =>
+        {
+            dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                authManager.InitializeFirebase();
+            }
+            else
+            {
+                Debug.Log("No se pudo resolver todas las dependencias de Firebase: " + dependencyStatus);
+            }
+        });
+    }
+
+}
---
+++ Mango/Assets/Scripts/Authentication/SignUpHandler.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading.Tasks;
+using Firebase.Auth;
+using Firebase.Extensions;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SignUpHandler : MonoBehaviour
+{
+    public TMP_InputField emailTextBox;
+    public TMP_InputField passwordTextBox;
+    public TMP_InputField confirmPasswordTextBox;
+    public Button signupButton;
+    public TMP_Text emailErrorText;
+    public TMP_Text passwordErrorText;
+    protected FirebaseAuth auth;
+    protected string displayName = "";
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        auth = FirebaseAuth.DefaultInstance;
+        signupButton.onClick.AddListener(() => Submit());
+    }
+
+    void Submit()
+    {
+        passwordErrorText.enabled = false;
+        if (passwordTextBox.text != confirmPasswordTextBox.text)
+        {
+            passwordErrorText.text = "La contraseña no coincide";
+            passwordErrorText.enabled = true;
+        }
+        else
+        {
+            CreateNewUser();
+        }
+    }
+    public Task CreateNewUser()
+    {
+        string email = emailTextBox.text;
+        string password = passwordTextBox.text;
+        Debug.Log("Creando usuario...");
+
+        return auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
+    }
+}
