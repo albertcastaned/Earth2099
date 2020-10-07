70d36d161e3cb52b9fd268f9a9dcfb5a961bd4c5
Alberto Castañeda <albcastar@gmail.com>
2020-10-07 13:22:44 -0500

    Cambiar color de panel de interfaz

--- Mango/Assets/Scripts/Authentication/SignUpHandler.cs
+++ Mango/Assets/Scripts/Authentication/SignUpHandler.cs
@@ -6,9 +6,16 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.Text.RegularExpressions;
 
 public class SignUpHandler : MonoBehaviour
 {
+    public const string MatchEmailPattern =
+        @"^(([\w-]+\.)+[\w-]+|([a-zA-Z]{1}|[\w-]{2,}))@"
+        + @"((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\."
+        + @"([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+        + @"([a-zA-Z]+[\w-]+\.)+[a-zA-Z]{2,4})$";
+
     public TMP_InputField emailTextBox;
     public TMP_InputField passwordTextBox;
     public TMP_InputField confirmPasswordTextBox;
@@ -27,16 +34,42 @@ public class SignUpHandler : MonoBehaviour
 
     void Submit()
     {
+        emailErrorText.enabled = false;
         passwordErrorText.enabled = false;
-        if (passwordTextBox.text != confirmPasswordTextBox.text)
+        bool valid = true;
+
+        if (String.IsNullOrEmpty(emailTextBox.text))
+        {
+            emailErrorText.text = "Este campo no puede estar vacio.";
+            emailErrorText.enabled = true;
+            valid = false;
+        }
+
+        if(!validateEmail(emailTextBox.text))
+        {
+            emailErrorText.text = "Este campo debe tener un correo valido.";
+            emailErrorText.enabled = true;
+            valid = false;
+        }
+
+        if (String.IsNullOrEmpty(passwordTextBox.text) || String.IsNullOrEmpty(confirmPasswordTextBox.text))
+        {
+            passwordErrorText.text = "Este campo no puede estar vacio.";
+            passwordErrorText.enabled = true;
+            valid = false;
+
+        }
+        else if (passwordTextBox.text != confirmPasswordTextBox.text)
         {
             passwordErrorText.text = "La contraseña no coincide";
             passwordErrorText.enabled = true;
+            valid = false;
+
         }
-        else
-        {
+
+        if(valid)
             CreateNewUser();
-        }
+        
     }
     public Task CreateNewUser()
     {
@@ -60,5 +93,13 @@ public class SignUpHandler : MonoBehaviour
             Debug.LogFormat("Firebase user created successfully: {0} ({1})",
                 newUser.DisplayName, newUser.UserId);
         });
+    }
+
+    public static bool validateEmail(string email)
+    {
+        if (email != null)
+            return Regex.IsMatch(email, MatchEmailPattern);
+        else
+            return false;
     }
 }
