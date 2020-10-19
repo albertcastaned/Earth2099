d1086ce6e28bc5edca60bbe9fb9021ae37f1135f
AdrizPaola <46755288+AdrizPaola@users.noreply.github.com>
2020-10-19 12:08:05 -0500

    feat(Map): Added map generation

---
+++ Mango/Assets/Scripts/LocationSelector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mapbox.Utils;
+using Mapbox.Unity.Map;
+using UnityEngine.Assertions;
+using UnityEngine;
+
+public class LocationSelector : MonoBehaviour
+{
+    
+    [SerializeField] private Vector2d[] locations;
+    [SerializeField] private int zoom = 16;
+
+    private void Awake(){
+        Assert.IsNotNull(locations);
+    }
+
+    // Start is called before the first frame update
+    void Start(){
+        AbstractMap map = FindObjectOfType<AbstractMap>();
+        if(map != null){
+            int index = Random.Range(0, locations.Length);
+            Vector2d location = locations[index];
+            map.Initialize(location, zoom);
+        }
+    }
+}
