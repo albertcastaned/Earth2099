fea0ad9aabd390ffc4c50cd1b07e05d0c4dd49d5
carlos <car_alf_98@icloud.com>
2020-10-28 22:13:22 -0600

    Añadir vida enemigos

---
+++ Mango/Assets/Scripts/Health_enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health_enemy : MonoBehaviour
+{
+    public int vida = 100;
+
+    public void RestLife(int amount)
+    {
+        vida -= amount;
+        Debug.Log(vida);
+
+        if (vida == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+}
---
+++ Mango/Assets/Scripts/makeDamage.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class makeDamage : MonoBehaviour
+{
+    public int cantidad = 10;
+
+    private void OnCollisionExit(Collision other)
+    {
+       // Debug.Log(other.gameObject.tag);
+        if(other.gameObject.tag == "Enemy")
+            other.gameObject.GetComponent<Health_enemy>().RestLife(cantidad);
+        Destroy(gameObject);
+    }
+    
+}
