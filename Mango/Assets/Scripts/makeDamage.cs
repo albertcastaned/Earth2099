645bd6aab1b554d3431f791686591a411c2dc0f6
carlos <car_alf_98@icloud.com>
2020-10-28 23:32:36 -0600

    Añadir barra de vida

--- Mango/Assets/Scripts/makeDamage.cs
+++ Mango/Assets/Scripts/makeDamage.cs
@@ -6,10 +6,10 @@ public class makeDamage : MonoBehaviour
 {
     public int cantidad = 10;
 
-    private void OnCollisionExit(Collision other)
+    private void OnCollisionEnter(Collision other)
     {
-       // Debug.Log(other.gameObject.tag);
-        if(other.gameObject.tag == "Enemy")
+       Debug.Log("Bala " + gameObject.name + " dispara a " + other.gameObject.name);
+       if(other.gameObject.tag == "Enemy")
             other.gameObject.GetComponent<Health_enemy>().RestLife(cantidad);
         Destroy(gameObject);
     }
