6757f1e898123fc581a406ee46cbb69aa2a6bbbd
carlos <car_alf_98@icloud.com>
2020-10-29 20:53:01 -0600

    Terminar caso

--- Mango/Assets/Scripts/ReceiveDamage.cs
+++ Mango/Assets/Scripts/ReceiveDamage.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 public class ReceiveDamage : MonoBehaviour
 {
     public int cantidad = 10;
-
-    private void OnCollisionEnter(Collision other)
-    { 
+    
+    private void OnTriggerEnter(Collider other)
+    {
         Debug.Log("Colisiona con " + other.gameObject.tag);
         if(other.gameObject.tag == "Player")
             other.gameObject.GetComponent<Health_Player>().RestLife(cantidad);
-        //Destroy(gameObject);
     }
 }
