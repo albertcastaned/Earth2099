16f671330323726517dbf1d1468f07d5ecca633c
carlos <car_alf_98@icloud.com>
2020-10-29 18:58:49 -0600

    Hacer vida barra 2

---
+++ Mango/Assets/Scripts/ReceiveDamage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReceiveDamage : MonoBehaviour
+{
+    public int cantidad = 10;
+
+    private void OnCollisionEnter(Collision other)
+    { 
+        Debug.Log("Colisiona con " + other.gameObject.tag);
+        if(other.gameObject.tag == "Player")
+            other.gameObject.GetComponent<Health_Player>().RestLife(cantidad);
+        //Destroy(gameObject);
+    }
+}
